87ecad6b3b24dbceb7ec40d53b6346d70661bd67
tsion203 <tsionmira167@gmail.com>
2026-05-04 13:51:17 +0300

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,17 @@
-export let cart = [
-{
+export let cart = 
+JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+cart = [{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2, 
 },{
     productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d", 
     quantity:1,
 }];
+}
+function saveToStorage(){
+    localStorage.setItem("cart",JSON.stringify(cart));
+}
 export function addTocart(productId){
      let matchingItem;
     cart.forEach((cartItem) => {
@@ -22,6 +28,7 @@ export function addTocart(productId){
         quantity: 1
     })
     }
+    saveToStorage();
 } 
 export function removeFromCart(productId){
      const newCart =[];
@@ -31,4 +38,5 @@ export function removeFromCart(productId){
         }
      } )
      cart = newCart;
+     saveToStorage();
 }
