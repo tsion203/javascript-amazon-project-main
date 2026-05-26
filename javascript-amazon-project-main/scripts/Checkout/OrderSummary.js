c7a84bb7275091e52d42e4cc156dcb06e71e43db
tsion203 <tsionmira167@gmail.com>
2026-05-26 16:50:36 +0300

    make payment summary interactive

--- javascript-amazon-project-main/scripts/Checkout/OrderSummary.js
+++ javascript-amazon-project-main/scripts/Checkout/OrderSummary.js
@@ -3,10 +3,8 @@ import {products, getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import{deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js"
-
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-deliveryDate.format('dddd MMMM D');  
+import { renderPaymentSummary } from "./PaymentSummary.js";
+ 
 export function renderOrderSummary(){
     let cartSummaryHTML = "";
     cart.forEach((cartItem) => {
@@ -101,6 +99,7 @@ export function renderOrderSummary(){
             `.js-cart-item-container-${productId}`
             );
             container.remove();
+            renderPaymentSummary();
     } );
     });
     document.querySelectorAll(".js-delivery-option").forEach((element) => {
@@ -108,6 +107,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
     } );
     });
 
