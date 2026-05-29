25b520beb0933ab82d0ebb0db30b20d8d0c851eb
tsion203 <tsionmira167@gmail.com>
2026-05-29 13:09:04 +0300

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import {renderOrderSummary} from "./Checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./Checkout/PaymentSummary.js";
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
