7916b237284ca39d92990e95eae0ad9c37511436
tsion203 <tsionmira167@gmail.com>
2026-06-02 02:19:55 +0300

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from "./Checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./Checkout/PaymentSummary.js";
 //import '../data/cart-class.js';
+import "../data/backend-practice.js";
 renderOrderSummary();
 renderPaymentSummary(); 
