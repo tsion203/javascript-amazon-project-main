2237a01aca7cf56cd97ca0c82ff44f447d2535f8
tsion203 <tsionmira167@gmail.com>
2026-07-01 10:29:02 +0300

    Make the tracking page fetch and calulate real time data

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -18,7 +18,13 @@ async function loadPage(){
             productDetails = details;
         
         }
-    })
+    });
+
+    const today = dayjs();
+    const orderTime = dayjs(order.orderTime);
+    const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+    const percentProgress = ((today - orderTime)/ (deliveryTime - orderTime)) *100;
+    
     const tarckingHTML = `
         <a class="back-to-orders-link link-primary" href="orders.html">
           View all orders
@@ -40,23 +46,27 @@ async function loadPage(){
         <img class="product-image" src="${product.image}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress < 50 ? 'current-status' : ''}">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label ${
+            (percentProgress >= 50 && percentProgress < 100) ? 'current-status' : ''} ">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label ${
+            percentProgress >=100 ? 'current-status' : ''}">
             Delivered
           </div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar"  style="width: ${percentProgress}%;"></div>
         </div>
       </div>
       `;
       document.querySelector('.js-order-tracking').innerHTML = tarckingHTML;
+
 }
 
 loadPage();
