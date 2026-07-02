8498850b86a12c3f80a49cbaa9bff450c9990a54
tsion203 <tsionmira167@gmail.com>
2026-07-03 00:19:21 +0300

    Make the search case insensitive and include keywords

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -12,7 +12,14 @@
 
    if(search) {
     filteredProducts = products.filter((product) => {
-        return product.name.includes(search);
+        let matchingKeyword = false;
+
+        product.keywords.forEach((keyword)=>{
+            if(keyword.toLowerCase().includes(search.toLowerCase())){
+                matchingKeyword = true;
+            }
+        })
+        return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase());
     });
     }
     filteredProducts.forEach((product) => {
