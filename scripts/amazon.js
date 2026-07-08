8c31dda81131de941ce603db6d9234e42fb4681c
tsion203 <tsionmira167@gmail.com>
2026-07-08 07:10:32 +0300

    Search by pressing Enter

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -100,6 +100,12 @@ document.querySelector('.js-search-button').addEventListener('click',() => {
     window.location.href = `amazon.html?search=${search}`
 
 })
+document.querySelector('.js-search-bar').addEventListener('keydown',(event) => {
+    if(event.key === 'Enter'){
+    const searchTerm = document.querySelector('.js-search-bar').value;
+    window.location.href = `amazon.html?search=${searchTerm}`
+    }
+})
 }
 
 renderProductsGrid();
