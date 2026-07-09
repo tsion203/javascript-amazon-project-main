59f19c7f9c523bfd1485f46e5de3313e0595ae18
tsion203 <tsionmira167@gmail.com>
2026-07-09 20:32:42 +0300

    Display delivered message if the delivery time is pasted today

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -24,6 +24,8 @@ async function loadPage(){
     const orderTime = dayjs(order.orderTime);
     const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
     const percentProgress = ((today - orderTime)/ (deliveryTime - orderTime)) *100;
+
+    const deliveredMessage = today < deliveryTime ? "Arriving on" : "Delivered on";
     
     const tarckingHTML = `
         <a class="back-to-orders-link link-primary" href="orders.html">
@@ -31,7 +33,7 @@ async function loadPage(){
         </a>
 
         <div class="delivery-date">
-          Arriving on ${
+          ${deliveredMessage} ${
             dayjs(productDetails.estimatedDeliveryTime).format("dddd, MMMM D")}
         </div>
 
